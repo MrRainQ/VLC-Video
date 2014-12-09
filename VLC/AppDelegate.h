ff1b1ca3936873823eda34238b2935afa8de7f51
MrRainQ <mrqiupeng126.com>
2014-12-09 16:39:28 +0800

    Initial Commit

---
+++ VLC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VLC
+//
+//  Created by sen5labs on 14-12-9.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VLCTests/VLCTests.m
@@ -0,0 +1,34 @@
+//
+//  VLCTests.m
+//  VLCTests
+//
+//  Created by sen5labs on 14-12-9.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VLCTests : XCTestCase
+
+@end
+
+@implementation VLCTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
